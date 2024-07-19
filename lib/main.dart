a46866a07033d7d26bce1f50430f518739075992
unknown <murad.zakaria888@gmail.com>
2024-07-19 21:48:21 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/consts.dart';
-import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+
 import 'package:notes_app/cubits/add_note_cubit/notes_cubit.dart';
 import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/simple_bloc_observer.dart';
